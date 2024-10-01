410385f66a749379e3922de0d5beb799465eecbf
WorksByDani <zapataparidaniela@gmail.com>
2024-10-01 18:21:50 -0400

    Estilos a Solicitud de Restablecimiento 100%

--- src/Pages/EnviarSolicitud.jsx
+++ src/Pages/EnviarSolicitud.jsx
@@ -6,7 +6,6 @@ function EnviarSolicitud() {
   const [email, setEmail] = useState('');
   const [rol, setRol] = useState('Estudiante'); 
   const [error, setError] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false); 
   const navigate = useNavigate(); 
 
   const handleSubmit = async (e) => {
@@ -18,7 +17,6 @@ function EnviarSolicitud() {
         rol       
       });
 
-      setIsModalOpen(true); 
       setError('');
 
     } catch (err) {
@@ -34,26 +32,31 @@ function EnviarSolicitud() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
       <div className='relative'>
-        <div className="absolute top-[10px] left-1/2 transform -translate-x-1/2 w-32 h-32 flex items-center justify-center rounded-full bg-white border-2 border-dark-blue">
-          <img src="/LogoMTis.svg" alt="Logo Color" className="w-28 h-26" />
+        <div className="absolute top-[10px] left-1/2 transform -translate-x-1/2 w-36 h-36 flex items-center justify-center rounded-full bg-gray-200">
+          <div className="w-32 h-32 flex items-center justify-center rounded-full bg-white border-2 border-shadow-custom">
+            <img src="/LogoMTis.svg" alt="Logo Color" className="w-28 h-26" />
+          </div>
         </div>
 
         <form onSubmit={handleSubmit} className="bg-white p-16 rounded-xl shadow-custom mt-20">
-          <div className="mb-4">
-            <h1 className="text-2xl font-bold mb-4">Enviar Solicitud de Restablecimiento</h1>
-            <label htmlFor="email" className="block text-gray-700">Correo Electrónico</label>
+          <div className="mb-4 mt-10">
+            <h1 className="text-2xl font-Montserrat font-bold mb-4">Enviar Solicitud de Restablecimiento</h1>
+            <p className="text-sm font-Montserrat font-medium max-w-md mx-auto mb-4">Introduzca el email asociado a la cuenta y te 
+              enviaremos las instrucciones para restablecer tu contraseña, revise su bandeja de correos.
+            </p>
+            <label htmlFor="email" className="block text-gray-700 font-Montserrat font-bold text-sm">Correo Electrónico</label>
             <input
               type="email"
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="border rounded w-full py-2 px-3 text-gray-700"
+              className="border-2 rounded-lg w-full py-2 px-3 text-gray-700"
             />
           </div>
 
           <div className="mb-6">
-            <label className="block text-gray-700 text-left">Selecciona tu rol</label>
+            <label className="block text-gray-700 font-Montserrat font-bold text-sm">Selecciona tu rol</label>
             <div className="flex justify-center items-left">
               <input
                 type="radio"
@@ -79,21 +82,11 @@ function EnviarSolicitud() {
           </div>
 
           <div className="flex justify-center mt-4">
-            <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">Enviar Correo</button>
+            <button type="submit" className="bg-sky-blue text-white text-sm font-Montserrat font-bold py-2 px-4 rounded">Enviar Correo</button>
           </div>
-        </form>
-        
-        {error && <p className="text-red-500 mt-4">{error}</p>}
+          {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
 
-        {isModalOpen && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded shadow-md w-80">
-              <h2 className="text-lg font-bold mb-4">Éxito</h2>
-              <p>Correo enviado exitosamente. Revise su bandeja de GMAIL.</p>
-              <button onClick={handleModalClose} className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">Aceptar</button>
-            </div>
-          </div>
-        )}
+        </form>
       </div>
     </div>
   );
