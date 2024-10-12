a5336b12fb85d99619fbc3695f5cc4a663174eb0
WorksByDani <zapataparidaniela@gmail.com>
2024-10-12 17:34:55 -0400

    Mostrar evaluaciones por temas

--- src/Components/Tablon.jsx
+++ src/Components/Tablon.jsx
@@ -1,44 +1,38 @@
 import React, { useContext, useEffect, useState } from "react";
-import { MdLibraryBooks } from "react-icons/md"; // Importar el ícono de libros
 import { useParams } from "react-router-dom";
-import { UserContext } from "../context/UserContext"; // Importar el UserContext
+import { UserContext } from "../context/UserContext"; 
+import { MdLibraryBooks } from "react-icons/md";
+import axios from "axios"; 
 
 const Tablon = () => {
-  const { cod_clase } = useParams(); // Obtener cod_clase de la URL
-  const [tareas, setTareas] = useState([]); // Cambiar el estado a "tareas"
-  const [cargando, setCargando] = useState(true);
-  const [error, setError] = useState(null);
-  const { user } = useContext(UserContext); // Obtener el usuario del contexto
-
-  // Simular datos de tareas por clases
-  const tareasPorClase = {
-    PRUEBA: [
-      { codigo_tarea: 1, nombre_tarea: "Tarea 1 - Introducción a React" },
-      { codigo_tarea: 2, nombre_tarea: "Tarea 2 - Estado y Props en React" },
-    ],
-    CJ7XWP: [
-      { codigo_tarea: 3, nombre_tarea: "Tarea 3 - Manejo de Efectos y Hooks" },
-      { codigo_tarea: 4, nombre_tarea: "Tarea 4 - Manejo de Rutas en React" },
-    ],
-  };
+  const { cod_clase } = useParams();
+  const [tareas, setTareas] = useState([]); 
+  const [cargando, setCargando] = useState(true); 
+  const [error, setError] = useState(null); 
+  const { user } = useContext(UserContext); 
 
   useEffect(() => {
-    setCargando(true);
-    // Simular una carga de datos de tareas según el código de clase después de 1 segundo
-    setTimeout(() => {
+    const fetchTareas = async () => {
+      setCargando(true);
       try {
-        if (tareasPorClase[cod_clase]) {
-          setTareas(tareasPorClase[cod_clase]); // Asignar las tareas correspondientes a la clase
-        } else {
-          setTareas([]); // Si no hay tareas para la clase, vaciar el estado
-        }
+        // Hacer una llamada al backend usando cod_clase
+        const response = await axios.get(`http://localhost:3000/evaluaciones/${cod_clase}`, {
+          headers: {
+            Authorization: `Bearer ${user.token}` 
+          }
+        });
+        setTareas(response.data); 
       } catch (error) {
-        setError("No se pudieron obtener las tareas.");
+        setError("Error al obtener las tareas."); 
       } finally {
-        setCargando(false); // Cambiar el estado de carga
+        setCargando(false); 
       }
-    }, 1000); // Simular un retardo de 1 segundo
-  }, [cod_clase]); // Ejecutar cuando cambie cod_clase
+    };
+
+    if (cod_clase) {
+      fetchTareas();
+    }
+  }, [cod_clase, user.token]); 
 
   if (cargando) {
     return <div>Cargando tareas...</div>;
@@ -52,58 +46,58 @@ const Tablon = () => {
     return <div>No está autenticado.</div>;
   }
 
+  // Agrupar las tareas por tema
+  const temasAgrupados = tareas.reduce((acc, t) => {
+    acc[t.nombre_tema] = t.evaluaciones;
+    return acc;
+  }, {});
+
+  // Verificar si no hay evaluaciones en absoluto
+  const noEvaluaciones = tareas.length === 0;
+
   return (
     <div className="p-2">
-      <h2 className="text-xl font-bold mb-4">Tablón de Evaluaciones</h2>
-
-      {user.rol === "docente" ? ( // Si el usuario es docente, mostrar vista del docente
-        <div>
-          {tareas.length > 0 ? (
-            tareas.map((tarea) => (
-              <div
-                key={tarea.codigo_tarea}
-                className="bg-light-blue rounded-lg p-4 flex justify-between items-center mb-4 shadow-md"
-              >
-                <div className="flex items-center">
-                  <span className="bg-white p-2 rounded-full text-semi-blue mr-4">
-                    <MdLibraryBooks size={32} />
-                  </span>
-                  <span className="text-lg font-medium">{tarea.nombre_tarea}</span>
-                </div>
-                <button className="bg-semi-blue text-white px-4 py-2 rounded-lg">
-                  Evaluar Tarea
-                </button>
-              </div>
-            ))
-          ) : (
-            <p>No hay evaluaciones registradas para esta clase.</p>
-          )}
-        </div>
-      ) : user.rol === "estudiante" ? ( // Si el usuario es estudiante, mostrar vista del estudiante
-        <div>
-          {tareas.length > 0 ? (
-            tareas.map((tarea) => (
-              <div
-                key={tarea.codigo_tarea}
-                className="bg-light-blue rounded-lg p-4 flex justify-between items-center mb-4 shadow-md"
-              >
-                <div className="flex items-center">
-                  <span className="bg-white p-2 rounded-full text-semi-blue mr-4">
-                    <MdLibraryBooks size={32} />
-                  </span>
-                  <span className="text-lg font-medium">{tarea.nombre_tarea}</span>
-                </div>
-                <button className="bg-semi-blue text-white px-4 py-2 rounded-lg">
-                  Ver Tarea
-                </button>
-              </div>
-            ))
-          ) : (
-            <p>No hay evaluaciones registradas para esta clase.</p>
-          )}
-        </div>
+      <h2 className="text-xl text-center font-bold mb-4">Tablón de Evaluaciones</h2>
+      {noEvaluaciones ? (
+        <div className="text-center text-gray-600">No existen evaluaciones aún.</div>
       ) : (
-        <p>No tiene un rol válido para acceder a esta información.</p>
+        Object.entries(temasAgrupados).map(([nombreTema, evaluaciones]) => (
+          <div key={nombreTema} className="mb-6">
+            <h3 className="text-lg font-semibold mb-2">{nombreTema}</h3>
+            {evaluaciones.length > 0 ? (
+              evaluaciones.map((e) => (
+                <div
+                  key={e.cod_evaluacion} 
+                  className="bg-light-blue rounded-lg p-4 flex justify-between items-center mb-4 shadow-md"
+                >
+                  <div className="flex items-center">
+                    <span className="bg-white p-2 rounded-full text-semi-blue mr-4">
+                      <MdLibraryBooks size={32} /> 
+                    </span>
+                    <div>
+                      <span className="text-lg font-medium">
+                        {e.descripcion_evaluacion} {/* Mostrar nombre de la evaluación */}
+                      </span>
+                      <div className="text-sm text-gray-600">{e.descripcion_evaluacion}</div> {/* Mostrar descripción de la evaluación */}
+                    </div>
+                  </div>
+                  {/* Botón para editar o realizar acción */}
+                  {user.rol === "docente" ? (
+                    <button className="bg-semi-blue text-white px-4 py-2 rounded-lg">
+                      Ver Evaluacion
+                    </button>
+                  ) : (
+                    <button className="bg-semi-blue text-white px-4 py-2 rounded-lg">
+                      Ver Evaluación
+                    </button>
+                  )}
+                </div>
+              ))
+            ) : (
+              <p>No hay tareas registradas para este tema.</p>
+            )}
+          </div>
+        ))
       )}
     </div>
   );
