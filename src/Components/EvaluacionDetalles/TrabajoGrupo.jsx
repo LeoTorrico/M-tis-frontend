3877febc4577d128e529be0a0414f66eedf30268
WorksByDani <zapataparidaniela@gmail.com>
2024-10-15 19:18:02 -0400

    Vista de entregas de evaluaciones conexion a bd 100%

--- src/Components/EvaluacionDetalles/TrabajoGrupo.jsx
+++ src/Components/EvaluacionDetalles/TrabajoGrupo.jsx
@@ -1,41 +1,65 @@
-import React, { useState } from "react";
-import { MdGroups } from "react-icons/md"; 
+import React, { useState, useEffect, useContext } from "react";
+import { MdGroups } from "react-icons/md";
+import { UserContext } from "../../context/UserContext"; 
 
 const TrabajoGrupo = ({ evaluacion }) => {
-  // Estado para los grupos
-  const [grupos] = useState([
-    { nombre: "Grupo 1", entregado: true, logotipo: null, nombre_largo: "Grupo de Desarrollo" },
-    { nombre: "Grupo 2", entregado: false, logotipo: null, nombre_largo: "Grupo de Diseño" },
-    { nombre: "Grupo 3", entregado: true, logotipo: null, nombre_largo: "Grupo de Marketing" },
-    { nombre: "Grupo 4", entregado: false, logotipo: null, nombre_largo: "Grupo de Investigación" },
-    { nombre: "Grupo 5", entregado: true, logotipo: null, nombre_largo: "Grupo de Ciencia" },
-    { nombre: "Grupo 6", entregado: false, logotipo: null, nombre_largo: "Grupo de Historia" },
-    { nombre: "Grupo 7", entregado: true, logotipo: null, nombre_largo: "Grupo de Literatura" },
-    { nombre: "Grupo 8", entregado: false, logotipo: null, nombre_largo: "Grupo de Música" },
-  ]);
 
+  const [gruposEntregados, setGruposEntregados] = useState([]);
   const [search1, setSearch1] = useState("");
   const [search2, setSearch2] = useState("");
+  const [loading, setLoading] = useState(true); 
+  const { user } = useContext(UserContext); 
 
-  // Filtrar grupos por búsqueda
-  const filteredGrupos1 = grupos.filter((grupo) =>
-    grupo.nombre.toLowerCase().includes(search1.toLowerCase())
+  useEffect(() => {
+    const fetchGrupos = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/evaluaciones/${evaluacion.cod_evaluacion}/entregas`, 
+          {
+            headers: {
+              Authorization: `Bearer ${user.token}`, 
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Error en la petición");
+        }
+        const data = await response.json();
+        setGruposEntregados(data); 
+        setLoading(false);
+      } catch (error) {
+        console.error("Error al obtener los grupos:", error);
+        setLoading(false);
+      }
+    };
+
+    if (evaluacion && user.token) {
+      fetchGrupos();
+    }
+  }, [evaluacion, user.token]); 
+
+  // Filtrar los grupos que han entregado
+  const filteredGruposEntregados = gruposEntregados.filter((grupo) =>
+    grupo.archivo_grupo && grupo.nombre_largo.toLowerCase().includes(search1.toLowerCase())
   );
 
-  const filteredGrupos2 = grupos.filter((grupo) =>
-    grupo.nombre.toLowerCase().includes(search2.toLowerCase())
+  // Filtrar los grupos que no han entregado
+  const filteredGruposNoEntregados = gruposEntregados.filter((grupo) =>
+    !grupo.archivo_grupo && grupo.nombre_largo.toLowerCase().includes(search2.toLowerCase())
   );
+
+  if (loading) {
+    return <p>Cargando grupos...</p>; 
+  }
 
   return (
     <div className="p-6 h-screen flex flex-col" style={{ maxHeight: 'calc(100vh - 60px)' }}>
       <h2 className="text-xl font-bold mb-4">{evaluacion.evaluacion}</h2>
-      <p className="mb-4">Aquí están los detalles del trabajo grupal para esta evaluación.</p>
+      <p className="mb-4">Aquí están los detalles del trabajo grupal que entregaron para esta evaluación.</p>
 
-      {/* Contenedor principal para columnas */}
       <div className="flex-grow flex overflow-hidden">
-        {/* Primer contenedor de búsqueda */}
         <div className="border p-4 rounded-lg shadow flex flex-col w-full mr-2">
-          <h3 className="font-bold mb-2">Buscar Grupos - Columna 1</h3>
+          <h3 className="font-bold mb-2">Grupos que entregaron</h3>
           <input
             type="text"
             placeholder="Buscar grupo..."
@@ -44,11 +68,11 @@ const TrabajoGrupo = ({ evaluacion }) => {
             className="border rounded p-2 w-full mb-4"
           />
           <div className="flex-grow overflow-y-auto" style={{ maxHeight: 'calc(100vh - 150px)' }}>
-            {filteredGrupos1.length > 0 ? (
-              filteredGrupos1.map((grupo, index) => (
+            {filteredGruposEntregados.length > 0 ? (
+              filteredGruposEntregados.map((grupo, index) => (
                 <div
                   key={index}
-                  className="bg-light-blue rounded-lg p-4 flex justify-between items-center mb-4"
+                  className="bg-blue-gray rounded-lg p-4 flex justify-between items-center mb-4"
                 >
                   <div className="flex items-center">
                     {grupo.logotipo ? (
@@ -64,20 +88,19 @@ const TrabajoGrupo = ({ evaluacion }) => {
                     )}
                     <span className="text-lg font-medium">{grupo.nombre_largo}</span>
                   </div>
-                  <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                  <button className="bg-dark-blue text-white px-4 py-2 rounded">
                     Evaluar
                   </button>
                 </div>
               ))
             ) : (
-              <p>No se encontraron grupos.</p>
+              <p>No se encontraron grupos que hayan entregado.</p>
             )}
           </div>
         </div>
 
-        {/* Segundo contenedor de búsqueda */}
         <div className="border p-4 rounded-lg shadow flex flex-col w-full">
-          <h3 className="font-bold mb-2">Buscar Grupos - Columna 2</h3>
+          <h3 className="font-bold mb-2">Grupos que no entregaron</h3>
           <input
             type="text"
             placeholder="Buscar grupo..."
@@ -86,11 +109,11 @@ const TrabajoGrupo = ({ evaluacion }) => {
             className="border rounded p-2 w-full mb-4"
           />
           <div className="flex-grow overflow-y-auto" style={{ maxHeight: 'calc(100vh - 150px)' }}>
-            {filteredGrupos2.length > 0 ? (
-              filteredGrupos2.map((grupo, index) => (
+            {filteredGruposNoEntregados.length > 0 ? (
+              filteredGruposNoEntregados.map((grupo, index) => (
                 <div
                   key={index}
-                  className="bg-light-blue rounded-lg p-4 flex justify-between items-center mb-4"
+                  className="bg-blue-gray rounded-lg p-4 flex justify-between items-center mb-4"
                 >
                   <div className="flex items-center">
                     {grupo.logotipo ? (
@@ -106,13 +129,13 @@ const TrabajoGrupo = ({ evaluacion }) => {
                     )}
                     <span className="text-lg font-medium">{grupo.nombre_largo}</span>
                   </div>
-                  <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                  <button className="bg-dark-blue text-white px-4 py-2 rounded">
                     Evaluar
                   </button>
                 </div>
               ))
             ) : (
-              <p>No se encontraron grupos.</p>
+              <p>No se encontraron grupos que no hayan entregado.</p>
             )}
           </div>
         </div>
