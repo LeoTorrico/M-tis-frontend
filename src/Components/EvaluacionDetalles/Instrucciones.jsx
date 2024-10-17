81fdaf5809330fc40db4fdf2fbace34730f79fae
WorksByDani <zapataparidaniela@gmail.com>
2024-10-16 22:25:50 -0400

    Visualizacion de archivos de la evaluacion

--- src/Components/EvaluacionDetalles/Instrucciones.jsx
+++ src/Components/EvaluacionDetalles/Instrucciones.jsx
@@ -18,20 +18,42 @@ const DetallesEvaluacion = ({ evaluacion }) => {
 
       {/* Contenedor de dos columnas */}
       <div className="border p-6 rounded-lg m-4 grid grid-cols-2 gap-4">
-        
         <div className="bg-gray-100 p-4 rounded-lg">
           <p className="text-xm">{evaluacion.descripcion_evaluacion}</p>
         </div>
 
         <div className="bg-gray-100 p-4 rounded-lg">
           {evaluacion.archivo_evaluacion ? (
-            <a
-              href={evaluacion.archivo_evaluacion}  // Suponiendo que evaluacion.archivo sea la URL del archivo
-              className="text-blue-600 underline"
-              download
-            >
-              Descargar archivo
-            </a>
+            <>
+              <a
+                href={`data:application/octet-stream;base64,${evaluacion.archivo_evaluacion}`}
+                className="text-blue-600 underline"
+                download
+              >
+                Descargar archivo
+              </a>
+
+              {/* Previsualización del archivo en Base64 */}
+              {evaluacion.archivo_evaluacion.startsWith("JVBERi0") ? (
+                // Si el archivo es PDF (cadenas Base64 de PDFs a menudo comienzan con "JVBERi0")
+                <iframe
+                  src={`data:application/pdf;base64,${evaluacion.archivo_evaluacion}`}
+                  title="Previsualización de PDF"
+                  className="w-full h-auto max-h-64 rounded"
+                  style={{ border: "none" }} 
+                />
+              ) : evaluacion.archivo_evaluacion.startsWith("/9j/") || // Para imágenes JPEG
+                evaluacion.archivo_evaluacion.startsWith("iVBORw0KGgo") ? ( // Para imágenes PNG
+                <img
+                  src={`data:image/jpeg;base64,${evaluacion.archivo_evaluacion}`}
+                  alt="Previsualización del archivo"
+                  className="mt-4"
+                  style={{ maxWidth: "100%", maxHeight: "400px" }}
+                />
+              ) : (
+                <p className="mt-4">Este tipo de archivo no se puede previsualizar.</p>
+              )}
+            </>
           ) : (
             <p>No hay archivo adjunto.</p>
           )}
