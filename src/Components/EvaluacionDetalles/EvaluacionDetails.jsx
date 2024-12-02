06c5a9223f31ef2af584def93837826aaa0c5522
WorksByDani <zapataparidaniela@gmail.com>
2024-12-02 06:58:48 -0400

    update: visualizar evaluacionDetails  vistas distintas docente/estudiante

--- src/Components/EvaluacionDetalles/EvaluacionDetails.jsx
+++ src/Components/EvaluacionDetalles/EvaluacionDetails.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { MdLibraryBooks, MdMoreVert } from 'react-icons/md';
 import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai';
+import { FaLink } from 'react-icons/fa';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 import MostarRubrica from './MostrarRubrica';
-import { FaLink } from 'react-icons/fa';
 import ArchivoAdjunto from "./ArchivoAdjunto";
 
-const EvaluacionDetails = ({ evaluacion, user, submitted, retrievedFile, isPastDueDate, handleFileChange, handleSubmit, linkInput, handleLinkChange, renderFilePreview, renderRetrievedFile }) => {
+const EvaluacionDetails = ({
+    evaluacion,
+    user,
+    submitted,
+    retrievedFile,
+    isPastDueDate,
+    handleFileChange,
+    handleSubmit,
+    linkInput,
+    handleLinkChange,
+    renderFilePreview,
+    renderRetrievedFile
+}) => {
     const [selectedFile, setSelectedFile] = useState(null);
     const [menuOpen, setMenuOpen] = useState(false);
     const [comentario, setComentario] = useState(null);
@@ -18,26 +30,19 @@ const EvaluacionDetails = ({ evaluacion, user, submitted, retrievedFile, isPastD
     useEffect(() => {
         const fetchComentarios = async () => {
             try {
-                const response = await axios.get(`http://localhost:3000/evaluaciones/${evaluacion.cod_evaluacion}/${cod_clase}/nota-total`, {
-                    headers: {
-                        Authorization: `Bearer ${user.token}`
+                const response = await axios.get(
+                    `http://localhost:3000/evaluaciones/${evaluacion.cod_evaluacion}/${cod_clase}/nota-total`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${user.token}`
+                        }
                     }
-                });
+                );
                 console.log('Respuesta del backend:', response.data);
 
-                if (response.data) {
-                    if (response.data.retroalimentacion && response.data.retroalimentacion.comentario) {
-                        setComentario(response.data.retroalimentacion.comentario);
-                    } else {
-                        setComentario(null);
-                    }
-
-                    if (response.data.comentario_individual) {
-                        setComentarioIndividual(response.data.comentario_individual);
-                    } else {
-                        setComentarioIndividual(null);
-                    }
-                }
+                const { retroalimentacion, comentario_individual } = response.data;
+                setComentario(retroalimentacion?.comentario || null);
+                setComentarioIndividual(comentario_individual || null);
             } catch (error) {
                 console.error('Error al obtener los comentarios:', error);
                 setComentario(null);
@@ -61,7 +66,7 @@ const EvaluacionDetails = ({ evaluacion, user, submitted, retrievedFile, isPastD
     };
 
     const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
+        setMenuOpen((prev) => !prev);
     };
 
     const handleDelete = async () => {
@@ -70,11 +75,14 @@ const EvaluacionDetails = ({ evaluacion, user, submitted, retrievedFile, isPastD
                 throw new Error('Token de autenticación no disponible');
             }
 
-            const response = await axios.delete(`http://localhost:3000/evaluaciones/${evaluacion.cod_evaluacion}`, {
-                headers: {
-                    Authorization: `Bearer ${user.token}`
+            const response = await axios.delete(
+                `http://localhost:3000/evaluaciones/${evaluacion.cod_evaluacion}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${user.token}`
+                    }
                 }
-            });
+            );
 
             console.log('Evaluación eliminada con éxito:', response.data);
             setMenuOpen(false);
@@ -106,9 +114,7 @@ const EvaluacionDetails = ({ evaluacion, user, submitted, retrievedFile, isPastD
                         {menuOpen && (
                             <div className="absolute right-0 mt-2 w-28 bg-white rounded-lg shadow-lg z-10">
                                 <button
-                                    onClick={() => {
-                                        setMenuOpen(false);
-                                    }}
+                                    onClick={() => setMenuOpen(false)}
                                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-lg w-full text-left"
                                 >
                                     Editar
@@ -126,9 +132,21 @@ const EvaluacionDetails = ({ evaluacion, user, submitted, retrievedFile, isPastD
             </div>
 
             <div className="border p-4 rounded-lg mt-0 m-4 flex-grow grid grid-cols-[minmax(0,2fr),minmax(0,1fr)] gap-4">
-                <div className="bg-blue-gray p-4 rounded-lg flex flex-col h-full">
+                <div className={`bg-blue-gray p-4 rounded-lg flex flex-col h-full ${user.rol === 'docente' ? 'col-span-2' : ''}`}>
                     <p className="text-xm font-semibold font-Montserrat">Descripción de la evaluación:</p>
                     <p className="text-xm font-Montserrat">{evaluacion.descripcion_evaluacion}</p>
+                    {user.rol === 'docente' && (
+                        <div className="flex justify-end items-center mt-2">
+                            <label className="text-xm font-semibold font-Montserrat mr-2">Fecha de entrega:</label>
+                            <p className={`text-xm font-semibold font-Montserrat`}>
+                                {new Date(evaluacion.fecha_fin).toLocaleDateString('es-ES', {
+                                    day: '2-digit',
+                                    month: 'short',
+                                    year: 'numeric'
+                                })}
+                            </p>
+                        </div>
+                    )}
                     <div className="col-span-2 overflow-y-auto mt-2">
                         <ArchivoAdjunto evaluacion={evaluacion} />
                     </div>
@@ -137,129 +155,108 @@ const EvaluacionDetails = ({ evaluacion, user, submitted, retrievedFile, isPastD
                     </div>
                 </div>
 
-                <div className="bg-blue-gray p-4 rounded-lg flex flex-col h-full">
-                    <p className={`text-xm font-semibold font-Montserrat ${isPastDueDate ? "text-red-400" : ""}`}>
-                        Fecha de entrega: {new Date(evaluacion.fecha_fin).toLocaleDateString('es-ES', {
-                            day: '2-digit',
-                            month: 'short',
-                            year: 'numeric'
-                        })}
-                    </p>
+                {user.rol !== 'docente' && (
+                    <div className="bg-blue-gray p-4 rounded-lg flex flex-col h-full">
+                        <p className={`text-xm font-semibold font-Montserrat ${isPastDueDate ? "text-red-400" : ""}`}>
+                            Fecha de entrega: {new Date(evaluacion.fecha_fin).toLocaleDateString('es-ES', {
+                                day: '2-digit',
+                                month: 'short',
+                                year: 'numeric'
+                            })}
+                        </p>
 
-                    {user.rol === "estudiante" ? (
-                        <form onSubmit={handleSubmit} className="flex flex-col h-full">
-                            {submitted && retrievedFile && renderRetrievedFile()}
+                        {user.rol === "estudiante" && (
+                            <form onSubmit={handleSubmit} className="flex flex-col h-full">
+                                {submitted && retrievedFile && renderRetrievedFile()}
 
-                            {!submitted && (
-                                <label className="inline-block w-full mt-2">
-                                    <button
-                                        type="button"
-                                        className="border border-gray-300 text-blue-500 bg-white py-2 px-4 rounded-lg w-full cursor-pointer flex items-center justify-center"
-                                        onClick={() => document.getElementById('file-input').click()}
-                                    >
-                                        <AiOutlinePlus className="mr-2 font-Montserrat" />
-                                        Añadir archivo
-                                    </button>
-                                    <input
-                                        id="file-input"
-                                        type="file"
-                                        onChange={onFileChange}
-                                        className="hidden"
-                                        accept="image/*,.pdf"
-                                    />
-                                </label>
-                            )}
-
-                            {selectedFile && renderFilePreview && (
-                                <div className="relative mt-4 flex-grow">
-                                    {renderFilePreview()}
-                                    {!submitted && (
-                                        <AiOutlineClose
-                                            className="absolute top-2 right-2 text-gray-500 cursor-pointer hover:text-gray-700"
-                                            onClick={handleRemoveFile}
-                                            size={24}
+                                {!submitted && (
+                                    <label className="inline-block w-full mt-2">
+                                        <button
+                                            type="button"
+                                            className="border border-gray-300 text-blue-500 bg-white py-2 px-4 rounded-lg w-full cursor-pointer flex items-center justify-center"
+                                            onClick={() => document.getElementById('file-input').click()}
+                                        >
+                                            <AiOutlinePlus className="mr-2 font-Montserrat" />
+                                            Añadir archivo
+                                        </button>
+                                        <input
+                                            id="file-input"
+                                            type="file"
+                                            onChange={onFileChange}
+                                            className="hidden"
+                                            accept="image/*,.pdf"
                                         />
-                                    )}
-                                </div>
-                            )}
-
-                            {!submitted && (
-                                <label className="inline-block w-full mt-2">
-                                    <span className="block font-semibold font-Montserrat">Enlace de entrega:</span>
-                                    <input
-                                        type="text"
-                                        value={linkInput}
-                                        onChange={(e) => handleLinkChange(e.target.value)}
-                                        className="border border-gray-300 p-2 rounded-lg w-full"
-                                        placeholder="Ingresa el enlace aquí (Opcional)"
-                                    />
-                                </label>
-                            )}
-
-                            {submitted && linkInput && (
-                                <div className="mt-2 bg-white p-2 border rounded-lg flex items-center">
-                                    <FaLink className="text-black-600 mr-2" />
-                                    <a
-                                        href={linkInput}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="text-gray-600 underline font-semibold"
-                                    >
-                                        {linkInput}
-                                    </a>
-                                </div>
-                            )}
-
-                            <button
-                                type="submit"
-                                className={`px-4 py-2 rounded-lg w-full mt-4 ${submitted ? 'bg-gray-400 text-white' : isPastDueDate ? 'bg-white text-red-500 border border-red-500' : 'bg-semi-blue text-white'}`}
-                                disabled={submitted || isPastDueDate}
-                            >
-                                {submitted ? "Entregado" : isPastDueDate ? "Sin entregar" : "Entregar"}
-                            </button>
-
-                            {comentario && (
-                                <div className="mt-2 p-3 bg-white rounded-lg shadow">
-                                    <p className="text-sm text-black font-Montserrat">
-                                        <strong>Comentario grupal:</strong> {comentario}
-                                    </p>
-                                </div>
-                            )}
-
-                            {comentarioIndividual && (
-                                <div className="mt-2 p-3 bg-white rounded-lg shadow">
-                                    <p className="text-sm text-black font-Montserrat">
-                                        <strong>Comentario individual:</strong> {comentarioIndividual}
-                                    </p>
-                                </div>
-                            )}
-                        </form>
-                    ) : (
-                        evaluacion.archivo_evaluacion ? (
-                            <>
-                                <a
-                                    href={`data:application/octet-stream;base64,${evaluacion.archivo_evaluacion}`}
-                                    className="text-blue-600 underline"
-                                    download
-                                >
-                                    Descargar archivo
-                                </a>
-
-                                {evaluacion.archivo_evaluacion.startsWith("JVBERi0") ? (
-                                    <iframe
-                                        src={`data:application/pdf;base64,${evaluacion.archivo_evaluacion}`}
-                                        title="Previsualización de PDF"
-                                        className="flex flex-col h-full"
-                                    />
-                                ) : (
-                                    <div>Vista previa no disponible para este tipo de archivo.</div>
+                                    </label>
                                 )}
-                            </>
-                        ) : (
-                            <div>No se ha subido un archivo de evaluación.</div>
-                        )
-                    )}
-                </div>
+
+                                {selectedFile && renderFilePreview && (
+                                    <div className="relative mt-4 flex-grow">
+                                        {renderFilePreview()}
+                                        {!submitted && (
+                                            <AiOutlineClose
+                                                className="absolute top-2 right-2 text-gray-500 cursor-pointer hover:text-gray-700"
+                                                onClick={handleRemoveFile}
+                                                size={24}
+                                            />
+                                        )}
+                                    </div>
+                                )}
+
+                                {!submitted && (
+                                    <label className="inline-block w-full mt-2">
+                                        <span className="block font-semibold font-Montserrat">Enlace de entrega:</span>
+                                        <input
+                                            type="text"
+                                            value={linkInput}
+                                            onChange={(e) => handleLinkChange(e.target.value)}
+                                            className="border border-gray-300 p-2 rounded-lg w-full"
+                                            placeholder="Ingresa el enlace aquí (Opcional)"
+                                        />
+                                    </label>
+                                )}
+
+                                {submitted && linkInput && (
+                                    <div className="mt-2 bg-white p-2 border rounded-lg flex items-center">
+                                        <FaLink className="text-black-600 mr-2" />
+                                        <a
+                                            href={linkInput}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="text-gray-600 underline font-semibold"
+                                        >
+                                            {linkInput}
+                                        </a>
+                                    </div>
+                                )}
+
+                                <button
+                                    type="submit"
+                                    className={`px-4 py-2 rounded-lg w-full mt-4 ${submitted ? 'bg-gray-400 text-white' : isPastDueDate ? 'bg-white text-red-500 border border-red-500' : 'bg-semi-blue text-white'}`}
+                                    disabled={submitted || isPastDueDate}
+                                >
+                                    {submitted ? "Entregado" : isPastDueDate ? "Sin entregar" : "Entregar"}
+                                </button>
+
+                                {comentario && (
+                                    <div className="mt-2 p-3 bg-white rounded-lg shadow">
+                                        <p className="text-sm text-black font-Montserrat">
+                                            <strong>Comentario grupal:</strong> {comentario}
+                                        </p>
+                                    </div>
+                                )}
+
+                                {comentarioIndividual && (
+                                    <div className="mt-2 p-3 bg-white rounded-lg shadow">
+                                        <p className="text-sm text-black font-Montserrat">
+                                            <strong>Comentario individual:</strong> {comentarioIndividual}
+                                        </p>
+                                    </div>
+                                )}
+
+                            </form>
+                        )}
+                    </div>
+                )}
             </div>
         </div>
     );
