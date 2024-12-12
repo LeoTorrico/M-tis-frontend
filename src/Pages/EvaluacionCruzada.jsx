f6afa4642483f0411dc64399f7f5e0c4fd2197a9
Leo Torrico <leonardotorrico50@gmail.com>
2024-12-12 01:47:29 -0400

    evaluacion de pares

--- src/Pages/EvaluacionCruzada.jsx
+++ src/Pages/EvaluacionCruzada.jsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useState, useContext } from "react";
+import { PiNewspaper } from "react-icons/pi";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "../context/UserContext";
+import Swal from "sweetalert2";
 
 const EvaluacionCruzada = () => {
-  const { cod_clase } = useParams();
+  const { cod_clase, cod_evaluacion } = useParams();
   const navigate = useNavigate();
   const { user } = useContext(UserContext);
-  const [grupoData, setGrupoData] = useState({
-    nombre_corto: "",
-    nombre_largo: "",
-    horario: {
-      dia_horario: "",
-      hora_inicio: "",
-      hora_fin: "",
-    },
-    integrantes: [],
-  });
+  const [grupoData, setGrupoData] = useState({});
   const [rubricScores, setRubricScores] = useState({});
   const [selectedStudentIndex, setSelectedStudentIndex] = useState(null);
-  const [comentario, setComentario] = useState("");
-  const [errorComentario, setErrorComentario] = useState("");
+  const [rubricDetails, setRubricDetails] = useState(null); // Nuevo estado
   const [error, setError] = useState("");
+  const [selectedRubricCode, setSelectedRubricCode] = useState(null);
+  const handleRubricChange = (index, value) => {
+    setRubricScores((prevScores) => ({
+      ...prevScores,
+      [index]: value,
+    }));
+  };
 
   useEffect(() => {
     // Validar el rol del usuario
@@ -46,6 +45,7 @@ const EvaluacionCruzada = () => {
         );
 
         setGrupoData(response.data);
+        console.log(grupoData);
       } catch (error) {
         console.error("Error al obtener los datos del grupo:", error);
       }
@@ -54,47 +54,80 @@ const EvaluacionCruzada = () => {
     if (cod_clase) {
       fetchGrupoData();
     }
-  }, [cod_clase, navigate, user]);
+  }, [cod_clase]);
 
-  const openRubricModal = (index) => {
+  const openRubricModal = async (index) => {
     setSelectedStudentIndex(index);
-    setComentario("");
-    setErrorComentario("");
-  };
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.get(
+        `http://localhost:3000/evaluaciones/${cod_evaluacion}/${cod_clase}/nota-total`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-  const handleRubricChange = (index, value) => {
-    setRubricScores((prev) => ({
-      ...prev,
-      [index]: value === "" ? "" : Number(value),
-    }));
-  };
+      const rubricas = response.data.rubricas || [];
+      console.log("Rubricas:", rubricas); // Verifica las rúbricas
 
-  const handleComentarioChange = (e) => {
-    const newComentario = e.target.value;
-    const wordCount = newComentario.trim().split(/\s+/).length;
+      if (rubricas.length > 0) {
+        const rubrica = rubricas[0]; // El primer elemento en el array
+        console.log("Cod Rubrica:", rubrica.cod_rubrica); // Verifica el valor de 'cod_rubrica'
 
-    if (wordCount < 3) {
-      setErrorComentario("El comentario debe tener al menos 3 palabras.");
-    } else if (wordCount > 100) {
-      setErrorComentario("El comentario no debe exceder las 100 palabras.");
-    } else {
-      setErrorComentario("");
+        setSelectedRubricCode(rubrica.cod_rubrica); // Guarda el cod_rubrica en el estado
+        setRubricDetails(rubrica.detalles || []); // Establecer detalles de la primera rúbrica
+      } else {
+        setRubricDetails([]); // Si no hay rúbricas, limpiar los detalles
+      }
+    } catch (error) {
+      console.error("Error al obtener los detalles de la rúbrica:", error);
     }
-
-    setComentario(newComentario);
   };
 
-  const saveRubricScores = () => {
-    const updatedIntegrantes = [...grupoData.integrantes];
-    updatedIntegrantes[selectedStudentIndex].score =
-      rubricScores[selectedStudentIndex];
-    updatedIntegrantes[selectedStudentIndex].comentario = comentario;
+  const saveRubricScores = async () => {
+    const estudiante = grupoData.integrantes[selectedStudentIndex];
 
-    setGrupoData((prev) => ({
-      ...prev,
-      integrantes: updatedIntegrantes,
-    }));
-    setSelectedStudentIndex(null);
+    // Obtenemos la nota correspondiente al estudiante y a la rúbrica
+    const nota = rubricScores[selectedStudentIndex]; // Nota ingresada
+
+    // Usamos el cod_rubrica guardado en el estado
+    const codRubrica = selectedRubricCode; // Usar el cod_rubrica del estado
+
+    const data = {
+      codEvaluacion: cod_evaluacion, // Código de la evaluación
+      codigoSis: estudiante.codigo_sis, // Código del estudiante
+      notas: [
+        {
+          codRubrica: codRubrica, // Usar el cod_rubrica guardado
+          nota: nota, // Nota ingresada para el estudiante
+        },
+      ],
+      comentario: "Buen desempeño en la evaluación.", // Comentario general
+    };
+
+    try {
+      const token = localStorage.getItem("token");
+      await axios.post("http://localhost:3000/evaluacion/calificar", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      // Mostrar el mensaje de éxito con SweetAlert2
+      Swal.fire({
+        icon: "success",
+        title: "¡Calificación guardada!",
+        text: "La calificación se ha guardado correctamente.",
+        confirmButtonText: "Aceptar",
+      });
+
+      // Si la calificación se guarda correctamente, podemos cerrar el modal y actualizar el estado
+      setSelectedStudentIndex(null);
+    } catch (error) {
+      console.error("Error al guardar la calificación:", error);
+    }
   };
 
   if (error) {
@@ -114,16 +147,14 @@ const EvaluacionCruzada = () => {
   return (
     <div className="flex flex-col w-full p-6 bg-white">
       <div className="bg-semi-blue text-white p-6 mb-6 rounded-lg">
-        <h2 className="text-2xl font-semibold">{grupoData.nombre_largo}</h2>
+        <h2 className="text-2xl font-semibold">Evaluación Cruzada</h2>
+        <p className="text-xl">{grupoData.nombre_largo}</p>
         <p className="text-xl">{grupoData.nombre_corto}</p>
-        <p className="text-lg mt-2">
-          Horario: {grupoData.horario.dia_horario}{" "}
-          {grupoData.horario.hora_inicio} - {grupoData.horario.hora_fin}
-        </p>
       </div>
 
       <div className="border border-black rounded-lg p-6 mb-6 overflow-x-auto">
         <div className="flex items-center mb-4">
+          <PiNewspaper className="text-2xl mr-2" />
           <h1 className="text-lg font-bold">Evaluación de Integrantes</h1>
         </div>
         <hr className="border-black my-2" />
@@ -141,6 +172,7 @@ const EvaluacionCruzada = () => {
               </div>
             ))}
           </div>
+
           <div>
             <h2 className="font-bold text-md mb-2 text-center">Nota</h2>
             <div className="flex flex-col space-y-2">
@@ -164,11 +196,10 @@ const EvaluacionCruzada = () => {
           </div>
         </div>
       </div>
-
       {selectedStudentIndex !== null &&
         grupoData.integrantes[selectedStudentIndex] && (
           <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-            <div className="bg-white p-4 rounded-lg shadow-lg max-w-4xl w-full mx-4 lg:mx-auto max-h-[90vh] overflow-y-auto">
+            <div className="bg-white p-0 rounded-t-lg rounded-b-lg shadow-lg max-w-4xl w-full mx-4 lg:mx-auto max-h-[90vh] overflow-y-auto">
               <h2 className="bg-[#3684DB] p-4 rounded-t-lg text-white font-bold w-full text-center">
                 Evaluar a{" "}
                 {grupoData.integrantes[selectedStudentIndex]?.nombre_estudiante}{" "}
@@ -179,46 +210,70 @@ const EvaluacionCruzada = () => {
               </h2>
 
               <div className="mb-4 p-4">
-                <input
-                  type="number"
-                  min="0"
-                  max="100"
-                  value={rubricScores[selectedStudentIndex] || ""}
-                  onChange={(e) =>
-                    handleRubricChange(selectedStudentIndex, e.target.value)
-                  }
-                  className="border border-gray-300 p-2 w-full bg-[#B3D6FF] rounded-xl mb-4"
-                  placeholder="Ingrese la nota (0-100)"
-                />
-
-                <div className="mb-4">
-                  <label className="font-bold mb-2 block">Comentarios</label>
-                  <textarea
-                    value={comentario}
-                    onChange={handleComentarioChange}
-                    placeholder="Ingrese un comentario..."
-                    className="border border-gray-300 p-2 w-full rounded-lg"
+                {rubricDetails && rubricDetails.length > 0 && (
+                  <div className="mb-4">
+                    <h3 className="font-bold">Detalles de la Rúbrica</h3>
+                    <table className="w-full border-collapse border border-gray-300 mt-2">
+                      <thead>
+                        <tr>
+                          <th className="border border-gray-300 px-4 py-2">
+                            Peso
+                          </th>
+                          <th className="border border-gray-300 px-4 py-2">
+                            Clasificación
+                          </th>
+                          <th className="border border-gray-300 px-4 py-2">
+                            Descripción
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {rubricDetails.map((detalle, idx) => (
+                          <tr key={idx}>
+                            <td className="border border-gray-300 px-4 py-2">
+                              {detalle.peso_rubrica}
+                            </td>
+                            <td className="border border-gray-300 px-4 py-2">
+                              {detalle.clasificacion_rubrica}
+                            </td>
+                            <td className="border border-gray-300 px-4 py-2">
+                              {detalle.descripcion}
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                )}
+                <div className="mb-4 p-4">
+                  <label className="font-bold mb-2 block">
+                    Ingrese la nota
+                  </label>
+                  <input
+                    type="number"
+                    min="0"
+                    max="100"
+                    value={rubricScores[selectedStudentIndex] || ""}
+                    onChange={(e) =>
+                      handleRubricChange(selectedStudentIndex, e.target.value)
+                    }
+                    className="border border-gray-300 p-2 w-full bg-[#B3D6FF] rounded-xl mb-4"
+                    placeholder="Ingrese la nota"
                   />
-                  {errorComentario && (
-                    <p className="text-red-500 mt-2">{errorComentario}</p>
-                  )}
                 </div>
+                <div className="mb-4 p-4"></div>
               </div>
 
-              <div className="bg-[#3684DB] p-4 rounded-b-lg flex justify-end">
+              <div className="bg-[#3684DB] p-3 rounded-b-lg flex justify-end gap-x-4">
                 <button
-                  className="bg-white text-[#3684DB] py-2 px-4 rounded-lg mr-2 border border-[#3684DB]"
+                  className="bg-white text-[#3684DB] px-6 py-2 rounded-lg hover:bg-red-500 hover:text-white"
                   onClick={() => setSelectedStudentIndex(null)}
                 >
                   Cancelar
                 </button>
                 <button
-                  className="bg-white text-[#3684DB] py-2 px-4 rounded-lg border border-[#3684DB]"
+                  className="bg-white text-[#3684DB] px-4 py-2 rounded-lg hover:bg-semi-blue hover:text-white"
                   onClick={saveRubricScores}
-                  disabled={
-                    !!errorComentario ||
-                    comentario.trim().split(/\s+/).length < 3
-                  }
                 >
                   Calificar
                 </button>
